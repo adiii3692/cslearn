c5f9ea940497922556e83307e20fc7e5f84b8c0d
adiii3692 <adinair0206@gmail.com>
2024-11-03 14:39:06 -0500

    Gonna master it this time

---
+++ Controllers/ItemsController.cs
@@ -0,0 +1,14 @@
+﻿using Microsoft.AspNetCore.Mvc;
+using Trial_Core_App.Models;
+
+namespace Trial_Core_App.Controllers
+{
+    public class ItemsController : Controller
+    {
+        public IActionResult Overview()
+        {
+            Item item = new Item { Name="Test Item"};
+            return View(item);
+        }
+    }
+}
---
+++ Models/Item.cs
@@ -0,0 +1,10 @@
+﻿namespace Trial_Core_App.Models
+{
+    public class Item
+    {
+        public int Id { get; set; }
+       public string Name { get; set; }
+        
+    }
+    // Let the user go to the /items/overview  URL route to access the item
+}
