c1be7147aeacbf750b4135b896a738cbbcd37121
adiii3692 <adinair0206@gmail.com>
2024-11-06 00:45:00 -0500

    Formatting

--- Controllers/ItemsController.cs
+++ Controllers/ItemsController.cs
@@ -7,24 +7,6 @@ namespace Trial_Core_App.Controllers
 {
     public class ItemsController : Controller
     {
-        //To learn how the basic action methods work that return a view
-        //public IActionResult Overview()
-        //{
-        //    Item item = new Item { Name="Test Item"};
-        //    return View(item);
-        //}
-
-        //Learn the various Action Parameters below: 
-
-        //Learn how to use URL Segments as action parameters
-        //public IActionResult Edit(int itemId)
-        //{
-        //    return Content("Id = " + itemId);
-        //}
-
-        //String query parameters are of the form: /items/edit?itemId=2
-
-
         //Store the context
         private readonly Trial_Core_AppContext _context;
 
